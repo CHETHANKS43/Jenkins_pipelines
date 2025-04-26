7cab9315c0f30762b0f3212327e27d116cd6e6c6
chethan <chethan@.com>
2025-04-26 12:25:02 +0530

    u

--- pipeline.php
+++ pipeline.php
@@ -1,15 +1,23 @@
+boolean runBuild=true
 pipeline {
     agent any
     stages{
         stage('checkout'){
+            if (runBuild)
             steps {
                 echo "checkout stage"
                 sleep 10
             }
+            runBuild=false
         }
         stage ('build'){
             steps {
                 echo 'checkout build'
+            }
+        }
+        stage ('deploy'){
+            steps {
+                echo 'checkout deploy'
             }
         }
     }
