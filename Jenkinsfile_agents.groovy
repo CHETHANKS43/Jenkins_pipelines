fa6d741fd07d22f4bb6fe7b9c811217a56cbe501
chethan <chethan@.com>
2025-04-26 16:29:22 +0530

    u

---
+++ Jenkinsfile_agents.groovy
@@ -0,0 +1,24 @@
+pipeline {
+    agent none
+    stages {
+        stage('Checkout') {
+            agent {
+                label 'slave2'
+            }
+            steps {
+                echo 'this is the first build'
+                sleep 20
+            }
+        }
+        stage ('build') {
+            agent {
+                lable 'slave1'
+            }
+            steps {
+                echo 'this is the build stage'
+                sleep 5
+            }
+        }
+    
+    }
+}
